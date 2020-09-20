28bf0e3479acd6cee5d0786b68c98d13d7256a43
Oluwatobi Bello <bellotobi10@gmail.com>
2020-09-20 05:07:25 -0400

    implimenting navigation bar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import AboutMe from "./components/AboutMe";
 import ContactMe from "./components/ContactMe";
 import HomePage from "./components/HomePage";
+import Navigation from "./components/Navigation";
 import Portfolio from "./components/Portfolio";
 import ServiceProvided from "./components/ServiceProvided";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <HomePage />
       <ServiceProvided />
       <Portfolio />
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Navigation.scss";
+
+export default function Navigation() {
+  return (
+    <div className="navigation">
+      <input
+        type="checkbox"
+        className="navigation__checkbox"
+        id="navi-toggle"
+      />
+
+      <label htmlFor="navi-toggle" className="navigation__button">
+        <span className="navigation__icon">&nbsp;</span>
+      </label>
+
+      <div className="navigation__background">&nbsp;</div>
+
+      <nav className="navigation__nav">
+        <ul className="navigation__list">
+          <li className="navigation__item">
+            <a href="#1" className="navigation__link">
+              Home Page
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#2" className="navigation__link">
+              Competencies
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#3" className="navigation__link">
+              Portfolio
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#4" className="navigation__link">
+              About Me
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#5" className="navigation__link">
+              Contact Me
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
