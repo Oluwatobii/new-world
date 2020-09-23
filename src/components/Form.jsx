230f7f0be3f6addb49e968419854e486ac3b0a54
Oluwatobi Bello <bellotobi10@gmail.com>
2020-09-23 18:55:07 -0400

    Added header to contact me page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,85 +12,90 @@ import {
 
 export default function Form() {
   return (
-    <section className="form-section">
-      <div className="form-container">
-        <div className="contactinfo">
-          <div>
-            <div className="contactinfo--box">
-              <h2>Contact Info</h2>
-              <ul className="info">
+    <div>
+      <div className="contact-me">
+        <h2 className="contact-me contact-me--text">Get in touch</h2>
+      </div>
+      <section className="form-section">
+        <div className="form-container">
+          <div className="contactinfo">
+            <div>
+              <div className="contactinfo--box">
+                <h2>Contact Info</h2>
+                <ul className="info">
+                  <li>
+                    <span>
+                      <FaMapMarkerAlt />
+                    </span>
+                    <span>Montreal, QC. Canada.</span>
+                  </li>
+                  <li>
+                    <span>
+                      <FaEnvelopeOpenText />
+                    </span>
+                    <span>bellotobi10@gmail.com</span>
+                  </li>
+                  <li>
+                    <span>
+                      <FaPhone />
+                    </span>
+                    <span>+1(438)-921-2579</span>
+                  </li>
+                </ul>
+              </div>
+              <ul className="sci">
                 <li>
-                  <span>
-                    <FaMapMarkerAlt />
-                  </span>
-                  <span>Montreal, QC. Canada.</span>
+                  <a href="#1">
+                    <FaTwitter />
+                  </a>
                 </li>
                 <li>
-                  <span>
-                    <FaEnvelopeOpenText />
-                  </span>
-                  <span>bellotobi10@gmail.com</span>
+                  <a href="#1">
+                    <FaLinkedinIn />
+                  </a>
                 </li>
                 <li>
-                  <span>
-                    <FaPhone />
-                  </span>
-                  <span>+1(438)-921-2579</span>
+                  <a href="#1">
+                    <FaGithub />
+                  </a>
                 </li>
               </ul>
             </div>
-            <ul className="sci">
-              <li>
-                <a href="#1">
-                  <FaTwitter />
-                </a>
-              </li>
-              <li>
-                <a href="#1">
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li>
-                <a href="#1">
-                  <FaGithub />
-                </a>
-              </li>
-            </ul>
+          </div>
+          <div className="contactform">
+            <form>
+              <h2>Send a Message</h2>
+              <div className="formbox">
+                <div className="inputbox w50">
+                  <input type="text" name="" required />
+                  <span>First Name</span>
+                </div>
+                <div className="inputbox w50">
+                  <input type="text" name="" required />
+                  <span>Last Name</span>
+                </div>
+                <div className="inputbox w50">
+                  <input type="text" name="" required />
+                  <span>Email Address</span>
+                </div>
+                <div className="inputbox w50">
+                  <input type="text" name="" required />
+                  <span>Moible Number</span>
+                </div>
+                <div className="inputbox w100">
+                  <textarea type="text" name="" required />
+                  <span>Write your message here...</span>
+                </div>
+                <div className="inputbox w100">
+                  <a href="#1" className="btn btn--dark-blue">
+                    Send
+                  </a>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-        <div className="contactform">
-          <form>
-            <h2>Send a Message</h2>
-            <div className="formbox">
-              <div className="inputbox w50">
-                <input type="text" name="" required />
-                <span>First Name</span>
-              </div>
-              <div className="inputbox w50">
-                <input type="text" name="" required />
-                <span>Last Name</span>
-              </div>
-              <div className="inputbox w50">
-                <input type="text" name="" required />
-                <span>Email Address</span>
-              </div>
-              <div className="inputbox w50">
-                <input type="text" name="" required />
-                <span>Moible Number</span>
-              </div>
-              <div className="inputbox w100">
-                <textarea type="text" name="" required />
-                <span>Write your message here...</span>
-              </div>
-              <div className="inputbox w100">
-                <a href="#1" className="btn btn--dark-blue">
-                  Send
-                </a>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
