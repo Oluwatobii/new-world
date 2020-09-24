9ea16b41eb2f0f73ec8529348c8d6046908d4aad
Oluwatobi Bello <bellotobi10@gmail.com>
2020-09-24 17:31:33 -0400

    Changed icon on service provided

--- src/components/ServiceProvided.jsx
+++ src/components/ServiceProvided.jsx
@@ -21,11 +21,11 @@ export default function ServiceProvided() {
               <img
                 className="imgBx--img"
                 alt="text"
-                src={require("../img/web-dev.jpg")}
+                src={require("../img/icons8-web-design-100.png")}
               />
+              <h2>Full-Stack Developer</h2>
             </div>
             <div className="content">
-              <h2>Full-Stack Developer</h2>
               <p>
                 I develop for the web, working with various stack technologies
                 such as; REact, Node, MongoDB, PostgreSQL, Ruby on Rails, and
@@ -38,11 +38,11 @@ export default function ServiceProvided() {
               <img
                 className="imgBx--img"
                 alt="text"
-                src={require("../img/mech-eng.jpg")}
+                src={require("../img/icons8-engineering-100.png")}
               />
+              <h2>Mechanical Engineer</h2>{" "}
             </div>
             <div className="content">
-              <h2>Mechanical Engineer</h2>
               <p>
                 Master's in Mechanical Engineering from Concordia University.
                 Designing that internal combustion engine won't be a problem.
@@ -54,11 +54,11 @@ export default function ServiceProvided() {
               <img
                 className="imgBx--img"
                 alt="text"
-                src={require("../img/cad-des.jpg")}
+                src={require("../img/icons8-design-100.png")}
               />
+              <h2>CAD Designer</h2>{" "}
             </div>
             <div className="content">
-              <h2>CAD Designer</h2>
               <p>
                 I draw cool 2D/3D CAD designs. Should you want to join the
                 Avengers and be the new iron-man, I'll help you with designing
@@ -71,11 +71,11 @@ export default function ServiceProvided() {
               <img
                 className="imgBx--img"
                 alt="text"
-                src={require("../img/colab-dev.jpg")}
+                src={require("../img/icons8-crowd-100.png")}
               />
+              <h2>Collaborative Development</h2>
             </div>
             <div className="content">
-              <h2>Collaborative Development</h2>
               <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Commodi tempora animi, voluptates alias natus pariatur
