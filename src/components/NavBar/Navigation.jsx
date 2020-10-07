04be5539da75e25181d42be947633d6921d91664
Oluwatobi Bello <bellotobi10@gmail.com>
2020-10-07 18:47:18 -0400

    Compressed Image

--- src/components/NavBar/Navigation.jsx
+++ src/components/NavBar/Navigation.jsx
@@ -6,17 +6,11 @@ import * as AiIcons from "react-icons/ai";
 
 import { NavBarData } from "./NavBarData";
 
-// import { TransitionGroup, CSSTransition } from "react-transition-group";
-
 export default function Navigation() {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
 
-  // const timeout = { enter: 800, exit: 400 };
-  // const [inProp, setInProp] = useState(false);
   return (
-    // <TransitionGroup component="div">
-    //   <CSSTransition in={inProp} timeout={timeout} className="my-node">
     <div className="nav-container">
       <div className="navbar">
         <a href="#1" className="menu-bars">
@@ -33,7 +27,6 @@ export default function Navigation() {
           {NavBarData.map((item, index) => {
             return (
               <li key={index} className={item.className}>
-                {/* <a href={item.path} onClick={() => setInProp(true)}> */}
                 <a href={item.path}>
                   {item.icon}
                   <span>{item.title}</span>
@@ -44,7 +37,5 @@ export default function Navigation() {
         </ul>
       </nav>
     </div>
-    //   </CSSTransition>
-    // </TransitionGroup>
   );
 }
